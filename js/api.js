b3393355321f30908977ce88b73e6d40259a0130
attendance-prog <business.work121314@gmail.com>
2026-03-03 21:32:16 +0530

    api.js

--- js/api.js
+++ js/api.js
@@ -7,3 +7,20 @@ function post(data){
     body:JSON.stringify(data)
   }).then(res => res.json());
 }
+async function callAPI(action, payload = {}) {
+  const response = await fetch(WEB_APP_URL, {
+    method: "POST",
+    body: JSON.stringify({
+      action: action,
+      ...payload
+    })
+  });
+
+  const text = await response.text();
+
+  try {
+    return JSON.parse(text);
+  } catch {
+    return text;
+  }
+}
